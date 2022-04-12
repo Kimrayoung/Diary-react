1ad09c6a45647d298d9c00e0a524a03e0b7222cb
Kimrayoung <rlafkdud122i@gmail.com>
2022-04-12 22:22:21 +0900

    react에서 Dom조작하기

--- simpleDiary/src/DiaryEditor.js
+++ simpleDiary/src/DiaryEditor.js
@@ -1,5 +1,12 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 const DiaryEditor = () => {
+    
+    //useRef함수를 호출하고 그 반환값을 authorInput에 담아줌
+    //그럼 useRef반환값으로 React.MutableRefObject가 저장이 됨
+    //MutableRefObject -> html즉, DOM요소를 접근할 수 있도록 도와줌
+    const authorInput = useRef();
+    const contentInput = useRef();
+
     //제목과 본문을 묶어주는 부분
     const [state, setState] = useState({
         author: "",
@@ -19,7 +26,23 @@ const DiaryEditor = () => {
     // const [content, setContent] = useState("");
     const handleSubmit = () => {
         console.log(state);
-        alert("저장 성공");
+        if(state.author.length < 1) {
+            // alert("작성자는 최소 1글자 이상 입력해주세요.");
+            //focus 위치
+            //useRef로 생성한 authorInput의 현재요소를 가르킴(current)
+            authorInput.current.focus();
+            return;
+        }
+
+        if (state.content.length < 5) {
+            // alert("본문은 최소 5글자 이상 입력해주세요.");
+            //focus 위치
+            contentInput.current.focus();
+            return;
+        }
+        else {
+            alert("저장 성공");
+        }
     }
 
     return (
@@ -27,18 +50,23 @@ const DiaryEditor = () => {
             <h2>오늘의 일기</h2>
             <div>
                 <input
+                    ref={authorInput}
                     name="author" 
                     value={state.author} 
+                    placeholder="작성자"
                     onChange={handleChangeState}/>
             </div>
             <div>
-                <textarea 
+                <textarea
+                    ref={contentInput} 
                     name="content"
+                    placeholder="일기"
                     value={state.content}
                     onChange={handleChangeState}
                 />
             </div>
             <div>
+                <span>오늘의 감정점수</span>
                 <select 
                     name="emotion"
                     value={state.emotion}
@@ -78,3 +106,12 @@ export default DiaryEditor;
 //textarea와 author는 state를 변화시키는 것이 동일함, 또한 자료형까지 같음 -> 하나의 state로 묶어줄 수 있음
 
 //jsx의 최상위 태그를 컴포넌트 이름과 맞춤으로 인해서 --> App.css에서 구분해주기위해서(어떤 컴포넌트에 적용될 건지를 바로 알 수 있음)
+
+//요즘은 alert을 띄우지 않는게 대세임
+//대신 alert -> focus효과를 주는 것이 좋음
+//focus를 주기 위해서는 어떤 DOM element에 효과를 줘야하는지 알고 있어야 함 즉, 어떤 element가 변화해야 하는지 알고 있어야 함 --> useRef사용
+//useRef -> 돔 요소를 선택하는 기능
+//useRef를 이용해서 생성한 reference객체는 현재 가르키는 값을 current라는 값으로 불러와서 사용할 수 있음
+//current속성을 가지고 있는 객체를 반환함
+//인자로 넘어온 초기값을 current속성에 할당함
+//html template부분에서 넘길 태그에 반드시 ref={}로 해서 바인딩을 시켜줘야 함
