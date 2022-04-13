bfc922ad55a8a65dd7100ad9697f94878dbad71d
Kimrayoung <rlafkdud122i@gmail.com>
2022-04-13 14:39:21 +0900

    section6-5 completed

--- simpleDiary/src/App.js
+++ simpleDiary/src/App.js
@@ -29,12 +29,11 @@ import DiaryList from './DiaryList';
   // ]
 
 const App = () => {
-  const [data, setData] = useState([]) //데이터의 변화를 감시 -> 일기 데이터를 감사(DiaryEditor, DiaryList가 함께 사용)
+  const [data, setData] = useState([])
 
-  const dataId = useRef(0)  //추가될 데이터의 ID를 확인하기 위해서(만약에 지금 data의 마지막 id가 3이라면 그 다음은 4)
+  const dataId = useRef(0);
 
-  const onCreate = (author, content, emotion) => {  //일기 배열에 새로운 데이터를 추가해주는 함수(이벤트 핸들러 --> 이벤트 처리함수)
-    //DiaryEditor에 작성한 author, content, emotion을 oncreate함수가 받아서 data를 업데이트 시키는 로직을 setData함수를 이용해서 onCreate함수 안에 작성할 것
+  const onCreate = (author, content, emotion) => {
     const created_date = new Date().getTime();
     const newItem = {
       author,
@@ -43,17 +42,20 @@ const App = () => {
       created_date,
       id: dataId.current
     };
-    dataId.current += 1; //1 증가시켜줘야 다음 id에 하나 증가한 값이 들어감
-    //dataId로 인해서 리렌더링 되면 안되고 해당 아이디를 조회, 수정만 하므로 useRef사용
+    dataId.current += 1; 
     setData([newItem, ...data ]);
   };
 
+  const onDelete = (targetId) => {  //App.js에서 직접 onDelete를 호출하는 것이 아니라서 targetId를 매개변수로 전달받아야 함
+    //데이터를 삭제하기 위해서는 DiaryItem에서 삭제버튼을 클릭했을 때 props로 전달받은 onDelete를 호출해서 데이터를 삭제
+    const NewDiaryList = data.filter((item) => item.id !== targetId);
+    setData(NewDiaryList);
+  }
+
   return (
     <div className="App">
-      {/* 앞의 onCreate가 DiaryEditor에서 props로 쓰일 이름, 그 이름에 App컴포넌트에서 정의해준 onCreate함수를 바인딩해줌
-      그러면 DiaryEditor에서 onCreate라는 이름은 함수를 내려줄 수 있다 */}
        <DiaryEditor onCreate={onCreate} />
-      <DiaryList diaryList={data} />
+      <DiaryList onDelete={onDelete} diaryList={data} />
     </div>
   );
 }
--- simpleDiary/src/DiaryList.js
+++ simpleDiary/src/DiaryList.js
@@ -1,7 +1,7 @@
 //일기 리스트(작성한 다이어리가 표시됨)
 import DiaryItem from "./DiaryItem";
 
-const DiaryList = ({ diaryList }) => {
+const DiaryList = ({ onDelete, diaryList }) => {
 
     return (
         <div className="DiaryList">
@@ -9,7 +9,7 @@ const DiaryList = ({ diaryList }) => {
             <h4>{diaryList.length}개의 일기가 있습니다.</h4>
             <div> {/*여기에 diaryList의 전체가 붙어야하고, 리스트의 요소들을 하나로 묶는 div이다(즉, DiaryItem을 하나로 묶는 div) */}
                 {diaryList.map((item) => (
-                    <DiaryItem key={`diaryitem_${item.id}`} {...item}></DiaryItem>
+                    <DiaryItem key={`diaryitem_${item.id}`} {...item} onDelete={onDelete}></DiaryItem>
                 ))}
             </div>
         </div>
@@ -21,47 +21,3 @@ DiaryList.defaultProps={
 }
 
 export default DiaryList;
-
-//학습목표 
-//React에서 리스트 데이터 추가하기 -> 배열을 이용한 React의 DiaryList에 아이템을 동적으로 추가해보기
-
-
-//DiaryEditor에서 데이터를 입력하면 DiaryList에 추가 해줘야 함
-//문제 : 같은 레벨에서는 데이터를 추가할 수가 없다. '
-//문제의 이유 : react에서는 단방향으로만 데이터가 흐른다 --> ex) App.js에서 DiaryList로 dummyList로 데이터가 흐른것 처럼
-//방법1. App데이터가 [data, setData]를 가지고 setData를 DiaryEditor로 내려주고 data를 DiaryList로 내려주면 됨 -> state 끌어올리기 방법
-
-//방법1의 흐름
-    //DiaryEditor에서 새로운 데이터(일기)가 추가되면 setData로 인해서 App컴포넌트의 data(배열)에 새로운 아이템이 추가될것
-    //그러면 이 새롭게 추가되서 업데이트 된 데이터를 DiaryList로 내려주면 됨 -> 전달받은 데이터가 변경됬으니까 DiaryList가 리렌더링
-    //App 컴포넌트는 DiaryEditor 컴포넌트가 어떤 일기를 언제 추가했는지 알아야 함 / 일기 추가와 같이 무언가 일이 발생했음을 '이벤트'라고함
-    //이 이벤트를 처리하는 함수를 --> 이벤트 핸들러라고함
-    //이 이벤트 핸들러는 새로운 일기 추가해주는 역할
-    //이 이벤트 핸들러는 App컴포넌트에도 존재함(onCreate)
-    //이 때 App 컴포넌트의 자식 컴포넌트인 DiaryEditor 컴포넌트에서 이벤트가 발생하면 해당 이벤트 핸들러를 통해 이벤트를 처리해야 하기 때문에, 이벤트 핸들러 자체를 prop으로 내려주는 것
-//방법1로 알수있는 것
-    //1. 리액트로 만든 컴포넌트들은 트리형태의 구조를 띈다
-    //2. 데이터는 위에서 아래로만 흐른다(단방향 데이터 구조)
-    //3. 이벤트(ex. 추가, 삭제)들은 아래에서 위로 흐른다(setData를 자식 컴포넌트로 전달한 다음에)
-        //DiaryEditor에서 새로운 일기를 작성하면 create라는 새로운 이벤트가 발생하는 것
-        //이 event는 부모 컴포넌트에서 전달한 setData를 호출해서 data를 변화시킴으로써 발생함
-
-
-//정리
-/* 
-DiaryEditor에서 생성한 일기를 DiaryList에서 보여주려면 DiaryEditor에서 생성된 데이터를 DiaryList로 넘겨줘야 한다.
-하지만 DiaryEditor과 DiaryList는 같은 레벨이기 때문에 데이터 전달이 불가능하다(데이터는 무조건 위에서 아래로 흐른다.)
-그러므로 DiaryList는 새로운 데이터를 App컴포넌트를 이용해서 받야야한다. 
-
-그리고 DiaryEditor은 데이터(배열)을 변화시켜줘야 한다. 즉, 새로운 일기를 작성해서 일기 저장하기를 누르면, 
-새로운 일기가 생성됬다는 즉, 새로운 일기 생성이라는 이벤트가 App으로 전달이 되고
-그로 인해서 props로 받은 onCreate함수를 호출
-onCreate함수가 실행되어 setState로 인해서 데이터(배열)이 변화한다. 
-
-즉, onCreate라는 이벤트 핸들러로 인해서 데이터가 변화함(생성이라는 이벤트가 발생하고 그 이벤트가 App으로 올라감)
-배열에 데이터를 추가하는 것 자체가 이벤트!!! 배열에 데이터를 추가해서 App에 올림 즉, 이벤트를 발생시키고 수행해서 App에 올림
-이벤트 처리를 통해서 setState가 변화된다 -> 이 setState의 변화를 App에서 감지
-새로운 일기를 작성해서 저장을 누르게 되면 App이 DiaryEditor에게 props로 전달한 setData를 호출 -> 그리고 setData로 인해서 데이터가 변경
-새로운 일기를 작성해서 저장을 누르면 그 때 App에서 DiaryEditor에게 props으로 전달한 setData를 호출하므로, 새로운 일기가 작성됬다는 것을 App에 알림(즉, 새로운 일기 생성이라는 이벤트가 App으로 전달이됨)
-이 이벤트가 
-*/
